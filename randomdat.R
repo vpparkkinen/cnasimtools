bc05deb4df050dc6729ba25dd2bac67467550faf
vpfof <parkkinenv@gmail.com>
2021-03-04 14:19:36 +0100

    init

---
+++ randomdat.R
@@ -0,0 +1,18 @@
+randomDat <- function(x = 6, condtype = c("csf", "asf"), 
+                      randomCondsArgs = 
+                        NULL,
+                      selectCasesArgs = NULL){
+  
+  ctype <- match.arg(condtype)
+  #margs <- list(...)
+  rargs <- ifelse(is.null(randomCondsArgs), list(x), list(x, randomCondsArgs))
+  mod <- if(ctype == "csf"){do.call(randomCsf, rargs)}else{
+    do.call(randomAsf, rargs)
+    }
+  
+  sargs <- ifelse(is.null(selectCasesArgs), list(mod), list(mod, selectCasesArgs))
+  ct <- do.call(selectCases, sargs)
+  df <- ct2df(ct)
+  attributes(df)$target <- mod
+  return(df)
+}
