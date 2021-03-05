d13574f91fbe1e47977294b4cc3d030a4c3d9dfd
vpfof <parkkinenv@gmail.com>
2021-03-05 09:23:48 +0100

    add imports/exports

--- R/randomdat.R
+++ R/randomdat.R
@@ -1,15 +1,17 @@
-randomDat <- function(x = 6, condtype = c("csf", "asf"), 
-                      randomCondsArgs = 
+#' @import cna
+#' @export
+randomDat <- function(x = 6, condtype = c("csf", "asf"),
+                      randomCondsArgs =
                         NULL,
                       selectCasesArgs = NULL){
-  
+
   ctype <- match.arg(condtype)
   #margs <- list(...)
   rargs <- ifelse(is.null(randomCondsArgs), list(x), list(x, randomCondsArgs))
   mod <- if(ctype == "csf"){do.call(randomCsf, rargs)}else{
     do.call(randomAsf, rargs)
     }
-  
+
   sargs <- ifelse(is.null(selectCasesArgs), list(mod), list(mod, selectCasesArgs))
   ct <- do.call(selectCases, sargs)
   df <- ct2df(ct)
